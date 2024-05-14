20023854c8a00fb43350210e44d09f5f5447742b
akita <zeal23310029@fsgcl.jp>
2024-05-14 11:48:32 +0900

    3.3 RectTest

--- 初級java2.5/src/RectTest.java
+++ 初級java2.5/src/RectTest.java
@@ -1,8 +1,19 @@
 
 public class RectTest extends MyFrame{
 	public void run() {
-		fillRect(30,50,200,100);
+		fillRect(30,40,50,100);
+		fillRect(130,40,50,100);
+		fillRect(30,200,150,10);
+		fillRect(90,160,30,30);
 		
+		fillRect(250,80,10,20);
+		fillRect(250,80,20,10);
+		fillRect(250,50,10,20);
+		fillRect(250,60,20,10);
+		fillRect(230,50,10,20);
+		fillRect(220,60,20,10);
+		fillRect(230,80,10,20);
+		fillRect(220,80,20,10);
 	}
 
 }
