495b5c9bc47bc028a795169734ff1a203a57250e
akita <zeal23310029@fsgcl.jp>
2024-05-14 10:47:41 +0900

    3.1完

--- 初級java2.5/src/start.java
+++ 初級java2.5/src/start.java
@@ -1,18 +1,9 @@
-
 public class start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-				
-		int x =2;
-		int y =3;
-		System.out.println(x*y);
-		
-		MyFrame frame1=new RectFrame();
-		frame1.setLocation(200,100);
-		
-		MyFrame frame2=new RectFrame();
-		frame2.setLocation(300,100);
+		System.out.println("プログラムを実行します");
+		MyFrame frame1=new RectTest();
 		
 		
 		
