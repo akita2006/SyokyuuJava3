736dfe901ad64722c88d4a1f42e23e96655bce5f
akita <zeal23310029@fsgcl.jp>
2024-05-14 12:11:12 +0900

    3.3setColor

--- 初級java2.5/src/RectTest.java
+++ 初級java2.5/src/RectTest.java
@@ -1,19 +1,32 @@
 
 public class RectTest extends MyFrame{
 	public void run() {
+		setColor(255,0,0);
 		fillRect(30,40,50,100);
-		fillRect(130,40,50,100);
-		fillRect(30,200,150,10);
-		fillRect(90,160,30,30);
 		
-		fillRect(250,80,10,20);
-		fillRect(250,80,20,10);
-		fillRect(250,50,10,20);
-		fillRect(250,60,20,10);
-		fillRect(230,50,10,20);
-		fillRect(220,60,20,10);
-		fillRect(230,80,10,20);
-		fillRect(220,80,20,10);
+		setColor(0,255,0);
+		fillRect(130,40,50,100);
+		
+		setColor(0,0,255);
+		fillRect(230,40,50,100);
+		
+		setColor(255,255,0);
+		fillRect(30,140,50,100);
+		
+		setColor(125,0,125);
+		fillRect(130,140,50,100);
+		
+		setColor(128,128,255);
+		fillRect(230,140,50,100);
+		
+		setColor(1,1,1);
+		fillRect(30,240,50,100);
+		
+		setColor(128,128,128);
+		fillRect(130,240,50,100);
+				
+		
+
 	}
 
 }
