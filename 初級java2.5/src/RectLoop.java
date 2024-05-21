490657b8510e0c18f277958f288db64b9ba97d94
akita <zeal23310029@fsgcl.jp>
2024-05-21 11:35:01 +0900

    追加問題１

--- 初級java2.5/src/RectLoop.java
+++ 初級java2.5/src/RectLoop.java
@@ -1,18 +1,19 @@
 
 public class RectLoop extends MyFrame{
 	public void run() {
-		int r=0;
-		int g=0;
-		int b=0;
-		int x=30 ;	
+		setColor(0,128,0);
+		int x=30;
+		int y=60;
+		int a=20;
+		int b=20;
 		for(int i=0; i<9; i++) {
-			System.out.println(x);			
-			setColor(r,g,b);
-			fillRect(x,60,10,100);	
-			x+=20;
-			r+=13;
-			g+=13;
-			b+=13;			
+			System.out.println(x);				
+			fillRect(x,y,a,b);	
+			x+=a;
+			y+=a;
+			a+=10;
+			b+=10;
+			
 		}
 		 
 	}
