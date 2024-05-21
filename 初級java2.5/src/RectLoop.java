f2ab37e790554f506b4385ffbde844a3bc8d0bfe
akita <zeal23310029@fsgcl.jp>
2024-05-21 12:29:12 +0900

    5.2 Animation

--- 初級java2.5/src/RectLoop.java
+++ 初級java2.5/src/RectLoop.java
@@ -2,11 +2,17 @@
 public class RectLoop extends MyFrame{
 	public void run() {
 		setColor(0,128,0);
-		int x=30 ;
+		int x=20 ;
+		int y=50 ;
+		int a=10;
+		int b=10;
 		for(int i=0; i<15; i++) {
 			System.out.println(x);
-			fillRect(x,80,10,100);	
-			x+=20;
+			fillRect(x,y,a,b);	
+			x+=a;
+			y+=a;
+			a+=10;
+			b+=20;
 		}
 		
 	}
