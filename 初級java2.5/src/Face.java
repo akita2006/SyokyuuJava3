cc90e462c1de21857db7728f75b623c30219aabb
akita <zeal23310029@fsgcl.jp>
2024-05-28 10:46:58 +0900

    6.2　x,y値の変化量の追加

--- 初級java2.5/src/Face.java
+++ 初級java2.5/src/Face.java
@@ -1,9 +1,12 @@
 
 public class Face {
-	int x,y;
-	public Face(int x,int y) {
+	int x,y,vx,vy;
+	public Face(int x,int y,int vx,int vy) {
 		this.x=x;
 		this.y=y;
+		this.vx=vx;
+		this.vy=vy;
+	
 		
 	}
 	
@@ -11,5 +14,10 @@ public class Face {
 		frame.fillOval(x,y,50,100);
 		frame.fillOval(x+100,y,50,100);
 		frame.fillRect(x,y+160,150,10);
+	}
+	
+	public void move() {
+		x+=vx;
+		y+=vy;
 	}
 }
