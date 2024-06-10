7d5b948c2a3c4a5c4d076c783e7190fdf032adee
akita <zeal23310029@fsgcl.jp>
2024-06-10 11:45:01 +0900

    訂正

--- 初級java2.5/src/Truck.java
+++ 初級java2.5/src/Truck.java
@@ -7,10 +7,10 @@ public class Truck extends Vehicle{
 	
 	public void draw(MyFrame frame){
 		frame.fillRect(x+60,y,20,30);		
-		frame.fillRect(x,y+20,80,10);
-		frame.fillOval(x+5,y+30,10,10);
-		frame.fillOval(x+15,y+30,10,10);
-		frame.fillOval(x+65,y+30,10,10);
+		frame.fillRect(x,y+20,80,15);
+		frame.fillOval(x+5,y+35,10,10);
+		frame.fillOval(x+15,y+35,10,10);
+		frame.fillOval(x+65,y+35,10,10);
 	}
 	
 	
